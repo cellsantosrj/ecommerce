580cbd99ca7387931e7b94025299292689136319
Marcelo Santos <marcelowebrs@gmail.com>
2019-06-08 18:57:35 -0300

    criando page admin e tpl admin

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
-
+use \Hcode\PageAdmin;
 
 
 $app = new Slim();
@@ -19,7 +19,31 @@ $app->get('/', function() {
 
 });
 
+$app->get('/admin/', function() {
+    
+	$page = new PageAdmin();
+        
+        $page->setTpl("index");
+
+});
+
+
 $app->run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
